12c3f9df5dfb23e3bf986dc556513cb8c5d73224
adisojeda <adisojeda1@gmail.com>
2022-04-14 22:51:23 -0700

    completed mindfulness dino game, still need to work on adding total count of breaths

--- chromedino.py
+++ chromedino.py
@@ -50,7 +50,8 @@ CLOUD = pygame.image.load(os.path.join("assets/Other", "Cloud.png"))
 
 BG = pygame.image.load(os.path.join("assets/Other", "Track.png"))
 
-FONT_COLOR=(0,0,0)
+FONT_COLOR = (0, 0, 0)
+
 
 class Dinosaur:
 
@@ -190,7 +191,8 @@ class Bird(Obstacle):
 
 
 def main():
-    global game_speed, x_pos_bg, y_pos_bg, points, obstacles
+    global game_speed, x_pos_bg, y_pos_bg, points, obstacles, breaths_per_second, start_time, mindfulness_points
+    breaths_per_second = 1/22
     run = True
     clock = pygame.time.Clock()
     player = Dinosaur()
@@ -199,23 +201,31 @@ def main():
     x_pos_bg = 0
     y_pos_bg = 380
     points = 0
+    start_time = datetime.datetime.now()
+    current_time = None
+    mindfulness_points = 0
     font = pygame.font.Font("freesansbold.ttf", 20)
     obstacles = []
     death_count = 0
     pause = False
 
     def score():
-        global points, game_speed
+        global points, game_speed, mindfulness_points, start_time, breaths_per_second
+        # update to mindfulness score
+        current_time = datetime.datetime.now()
+        mindfulness_points = round(
+            (current_time - start_time).seconds * breaths_per_second, 2)
         points += 1
         if points % 100 == 0:
-            game_speed += 1
-        current_time = datetime.datetime.now().hour
+            game_speed += 0.05
+
         with open("score.txt", "r") as f:
-            score_ints = [int(x) for x in f.read().split()]  
+            score_ints = [int(x) for x in f.read().split()]
             highscore = max(score_ints)
             if points > highscore:
-                highscore=points 
-            text = font.render("High Score: "+ str(highscore) + "  Points: " + str(points), True, FONT_COLOR)
+                highscore = points
+            text = font.render("Mindfulness Count: " + str(mindfulness_points) +
+                               " High Score: " + str(highscore) + "  Points: " + str(points), True, FONT_COLOR)
         textRect = text.get_rect()
         textRect.center = (900, 40)
         SCREEN.blit(text, textRect)
@@ -239,9 +249,10 @@ def main():
         nonlocal pause
         pause = True
         font = pygame.font.Font("freesansbold.ttf", 30)
-        text = font.render("Game Paused, Press 'u' to Unpause", True, FONT_COLOR)
+        text = font.render(
+            "Game Paused, Press 'u' to Unpause", True, FONT_COLOR)
         textRect = text.get_rect()
-        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT  // 3)
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 3)
         SCREEN.blit(text, textRect)
         pygame.display.update()
 
@@ -299,46 +310,62 @@ def main():
 
 
 def menu(death_count):
-    global points
+    """Welcome to the Mindfulness Dyno Game
+    - Focus on a single breath per mindfulness point
+    - Press any Key to Start"""
+    global points, start_time
     global FONT_COLOR
     run = True
     while run:
         current_time = datetime.datetime.now().hour
         if 7 < current_time < 19:
-            FONT_COLOR=(0,0,0)
+            FONT_COLOR = (0, 0, 0)
             SCREEN.fill((255, 255, 255))
         else:
-            FONT_COLOR=(255,255,255)
+            FONT_COLOR = (255, 255, 255)
             SCREEN.fill((128, 128, 128))
         font = pygame.font.Font("freesansbold.ttf", 30)
 
         if death_count == 0:
             text = font.render("Press any Key to Start", True, FONT_COLOR)
+            start_time = datetime.datetime.now()
         elif death_count > 0:
             text = font.render("Press any Key to Restart", True, FONT_COLOR)
             score = font.render("Your Score: " + str(points), True, FONT_COLOR)
             scoreRect = score.get_rect()
             scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
             SCREEN.blit(score, scoreRect)
-            f = open("score.txt", "a")
+
+            # mindfulness score
+            mindfulness_score = font.render(
+                "Mindfulness Score: " + str(round(mindfulness_points * 4 / 12, 2)), True, FONT_COLOR)
+            mindfulness_scoreRect = mindfulness_score.get_rect()
+            mindfulness_scoreRect.center = (
+                SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 150)
+            SCREEN.blit(mindfulness_score, mindfulness_scoreRect)
+
+            f = open("score.txt", "w")
             f.write(str(points) + "\n")
             f.close()
             with open("score.txt", "r") as f:
                 score = (
                     f.read()
                 )  # Read all file in case values are not on a single line
-                score_ints = [int(x) for x in score.split()]  # Convert strings to ints
+                # Convert strings to ints
+                score_ints = [int(x) for x in score.split()]
             highscore = max(score_ints)  # sum all elements of the list
             hs_score_text = font.render(
                 "High Score : " + str(highscore), True, FONT_COLOR
             )
             hs_score_rect = hs_score_text.get_rect()
-            hs_score_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 100)
+            hs_score_rect.center = (
+                SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 100)
             SCREEN.blit(hs_score_text, hs_score_rect)
         textRect = text.get_rect()
         textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         SCREEN.blit(text, textRect)
-        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
+        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 -
+                    20, SCREEN_HEIGHT // 2 - 140))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
